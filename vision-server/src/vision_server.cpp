16009b964dd9564b060731b35a38b0624ccd9265
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-07-18 10:23:51 -0400

    made kinect move

--- vision-server/src/vision_server.cpp
+++ vision-server/src/vision_server.cpp
@@ -9,9 +9,14 @@ class Controller : public FreenectDevice {
 
 int main() 
 {
-    std::cout << "test\n";
     Freenect::Freenect freenect;
     Freenect::FreenectDevice* device = &freenect.createDevice<Freenect::FreenectDevice>(0);
-    device->setLed(LED_RED);
+    device->setLed(LED_GREEN);
+    while (1) {
+        double degrees;
+        std::cout << "degrees to move: ";
+        std::cin >> degrees;
+        device->setTiltDegrees(degrees);
+    }
     return 0;
 }
