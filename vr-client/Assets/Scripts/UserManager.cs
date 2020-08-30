4f2c6da9e61dcd85f439026cc3b707cb22ace367
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2020-08-30 05:59:16 -0400

    removed unused script

--- vr-client/Assets/Scripts/UserManager.cs
+++ vr-client/Assets/Scripts/UserManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PcUserManager : MonoBehaviour
+public class UserManager : MonoBehaviour
 {
 
     public int currentAngle = 0;
