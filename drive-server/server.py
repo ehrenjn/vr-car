04ca9e3976a54c516f355390f77d65b558c39e5d
Your Name <you@example.com>
2020-12-01 00:46:43 +0000

    made working script

--- drive-server/server.py
+++ drive-server/server.py
@@ -2,19 +2,22 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD) # use BOARD pin numbering so that we can switch out pis and the same pins work (hopefully)
 
-# pin 12 and 13 are mirrored (they always have the same output) and are 2 of the only 4 pwm pins, so they will be used for output
+# pin 12 and 32 are mirrored (they always have the same output) and are 2 of the only 4 pwm pins, so they will be used for output
 GPIO.setup(12, GPIO.OUT)
-GPIO.setup(33, GPIO.OUT)
+GPIO.setup(32, GPIO.OUT)
 
-pwm = GPIO.PWM(12, 500) # start pwm at 500hz
-pwm = GPIO.PWM(33, 500) # start pwm at 500hz
+pwm1 = GPIO.PWM(12, 500) # start pwm at 500hz
+pwm2 = GPIO.PWM(32, 500) # start pwm at 500hz
 
 
-pwm.start(0)
 duty = 0
+pwm1.start(duty)
+pwm2.start(duty)
 while duty <= 100:
-    pwm.ChangeDutyCycle(duty)
+    pwm1.ChangeDutyCycle(duty)
+    pwm2.ChangeDutyCycle(duty)
     duty = int(input("new duty cycle (0 - 100): "))
 
-pwm.stop()
+pwm1.stop()
+pwm2.stop()
 GPIO.cleanup()
