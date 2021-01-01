0256ed9cd8db77da44d4fba20b661b4553316f2c
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2020-12-31 19:43:41 -0500

    fixed stuff

--- car-code/launch_servers_local.sh
+++ car-code/launch_servers_local.sh
@@ -2,11 +2,11 @@ vr_car_home="${HOME}/vr-car/" # have to use $HOME instead of ~ because it won't 
 
 # kill servers first
 echo "killing running servers"
-cd "${vr_car_home}/car-code/vision-server" && make kill
+cd "${vr_car_home}/car-code/vision-server/src" && make kill
 /bin/bash ${vr_car_home}/car-code/kill_server.sh "python3 server.py"
 
 # then run servers
 echo "restarting drive server"
 cd "${vr_car_home}/car-code/drive-server" && python3 ./server.py &
 echo "restarting vision server"
-cd "${vr_car_home}/car-code/vision-server" && ./vision_server &
+cd "${vr_car_home}/car-code/vision-server/src" && ./vision_server &
