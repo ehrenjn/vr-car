122af695a9fa41b459c8357803d7e56cac599436
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2021-02-06 22:56:03 -0500

    added comments, changed to run at 2 hz

--- slam/rtabmap_source/NoEventsExample/run_example.sh
+++ slam/rtabmap_source/NoEventsExample/run_example.sh
@@ -9,4 +9,7 @@ export DISPLAY="${1}:0.0"
 cd /root/no_events/NoEventsExample \
     && cmake . \
     && make \
-    && ./noEventsExample 20 2 10 stereo_20Hz stereo_20Hz stereo_20Hz/left stereo_20Hz/right
+    && ./noEventsExample 2 1 2 stereo_20Hz stereo_20Hz stereo_20Hz/left_NEW stereo_20Hz/right_NEW
+    # args: camera_rate (fps) odom_update (frames per update) map_update (hz) calibration_dir calibration_name path_left_images path_right_images
+    # default values: 20 2 10
+    # good values for 2 fps data: 2 1 2
