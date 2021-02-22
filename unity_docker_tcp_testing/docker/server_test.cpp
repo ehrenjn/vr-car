78f99094ce83105a42484543dd962f725d96908c
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2021-02-22 06:45:13 -0500

    remove debug pritns

--- unity_docker_tcp_testing/docker/server_test.cpp
+++ unity_docker_tcp_testing/docker/server_test.cpp
@@ -3,7 +3,7 @@
 
 
 int main() {
-    std::cout << "server main startedn" << std::endl;
+    std::cout << "server main started" << std::endl;
 
     TCPServer server(7787);
     uint8_t messageArray[10];
@@ -15,18 +15,14 @@ int main() {
     std::cout << "client connected" << std::endl;
 
     Message received = server.receiveMessage(receivedData);
-	std::cout << "loop" << std::endl;
     for (unsigned int i = 0; i < received.getDataLength(); i++) {
         std::cout << received.getStartOfData()[i];
-		std::cout << "," << std::endl;
     }
     std::cout << std::endl;
-	std::cout << "resp" << std::endl;
 
     uint8_t toSend[] = {'r', 'e', 's', 'p', 'o', 'n', 's', 'e'};
     message.setData(0, toSend, sizeof(toSend));
     server.sendMessage(message);
-	std::cout << "done" << std::endl;
 
     return 0;
 }
